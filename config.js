e3826353f7201ea69c582a82e1ad8902968f79fe
Yuuuu <75476389+abo2319@users.noreply.github.com>
2026-04-10 15:04:18 +0800

    Update Google OAuth Client ID in config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // ─── 請在這裡填入你的 Google OAuth Client ID ───────────────────
 const CONFIG = {
-  CLIENT_ID: 'YOUR_GOOGLE_CLIENT_ID_HERE'
+  CLIENT_ID: '832186580342-vp1jh1m1c95duqo4gp879bg24htitvts.apps.googleusercontent.com'
 };
